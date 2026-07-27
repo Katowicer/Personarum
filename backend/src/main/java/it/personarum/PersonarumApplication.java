4ce32e2c0d5f60fc72e31aaa37d02faa32b18063
Oktavio Jashari <oktavio.jashari@polimi.it>
2026-07-27 22:52:26 +0200

    chore(backend): init Spring Boot app

---
+++ backend/src/main/java/it/personarum/PersonarumApplication.java
@@ -0,0 +1,13 @@
+package it.personarum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonarumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonarumApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/it/personarum/PersonarumApplicationTests.java
@@ -0,0 +1,14 @@
+package it.personarum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class PersonarumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
